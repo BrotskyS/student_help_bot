7918488c6977f467cd47ff8ef7b903cbc299d0aa
masterboda <magisterboda.go.ua@gmail.com>
2020-04-07 23:07:14 +0300

    SQL string style changed

--- db.py
+++ db.py
@@ -12,15 +12,23 @@ mycursor = connection.cursor()
 mycursor.execute('CREATE DATABASE IF NOT EXISTS telebot')
 mycursor.execute('USE telebot')
 
-mycursor.execute('CREATE TABLE IF NOT EXISTS homework( '
-						+'id INT AUTO_INCREMENT PRIMARY KEY,'
-						+'message_id INT NULL,'
-						+'user_id INT,'
-						+'from_chat_id INT,'
-						+'media_group_id TEXT NULL,'
-						+'date VARCHAR(10))')
+mycursor.execute(
+	"""
+	CREATE TABLE IF NOT EXISTS homework( 
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	message_id INT NULL,
+	user_id INT,
+	from_chat_id INT,
+	media_group_id TEXT NULL,
+	date VARCHAR(10))
+	"""
+)
 
-mycursor.execute('CREATE TABLE IF NOT EXISTS media( '
-						+'id INT AUTO_INCREMENT PRIMARY KEY,'
-						+'media_group_id TEXT,'
-						+'files TEXT)')
+mycursor.execute(
+	"""
+	CREATE TABLE IF NOT EXISTS media( 
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	media_group_id TEXT,
+	files TEXT)
+	"""
+)
