73d91241984ae99223d983e7b55c287837e75c80
masterboda <magisterboda.go.ua@gmail.com>
2020-04-10 12:26:43 +0300

    date regex fixed

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 import re
 
-date_pattern = re.compile(r'(?:\s+)?([0-2][1-9]|[3][01])[-\/.]([0][1-9]|[1][12])(?:\s+)')
+date_pattern = re.compile(r'(?:\s+)?([0-2][1-9]|[3][01])[-\.\/]([0][1-9]|[1][12])(?:\s+)')
 
 def find_date(text):
-	pattern = r'([0-2][1-9]|[3][01])[-\/.]([0][1-9]|[1][12])(?:\s+)?$'
+	pattern = r'([0-2][1-9]|[3][01])[-\.\/]([0][1-9]|[1][12])(?:\s+)?$'
 	match = re.search(pattern, text)
 	return f'{match[1]}/{match[2]}' if match else None
